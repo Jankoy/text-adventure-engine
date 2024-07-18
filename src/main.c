5773f745d30b6c65fc00e680e53250331fe9d050
Jankoy <jankoypublic@gmail.com>
2024-07-18 15:16:26 -0400

    Worked on text formatting, and a dated message log.

--- src/main.c
+++ src/main.c
@@ -7,7 +7,10 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <curses.h>
+#include <time.h>
 #include <term.h>
+
+#include "nob.h"
 
 void get_term_size(int *cols, int *rows) {
   char const *const term = getenv("TERM");
@@ -67,15 +70,17 @@ done:
 }
 
 #define ESC "\033"
+
 #define CLEAR_SCREEN ESC"[2J"
 #define RESET_CURSOR ESC"[H"
+
 #define MOVE_CURSOR(column, line) ESC"[%d;%dH", (line), (column)
 
-static volatile sig_atomic_t running = true;
+#define COLOR_GRAY  ESC"[2;37m"
+#define COLOR_RESET ESC"[0m"
 
 static void sig_handler(int _) {
     (void)_;
-    running = false;
     printf(RESET_CURSOR);
     printf(CLEAR_SCREEN);
     exit(0);
@@ -84,37 +89,69 @@ static void sig_handler(int _) {
 #define INPUT_BUF_CAP (256 + 1)
 static char input_buf[INPUT_BUF_CAP] = {};
 
-static const char *const test_message_log[] = {
-  "First message!",
-  "Second message!",
-  "Third message!"
-};
+typedef struct {
+  time_t time;
+  const char *msg;
+} message_t;
+
+static struct {
+  message_t *items;
+  size_t count;
+  size_t capacity;
+} message_log = {};
+
+static inline void put_many_char(char c, size_t count) {
+  for (size_t i = 0; i < count; ++i)
+    putchar(c);
+}
+
+static inline void put_many_str(char *s, size_t count) {
+  for (size_t i = 0; i < count; ++i)
+    printf(s);
+}
+
+#define FORMAT_TIME_BUF_CAP 64
+static inline const char *format_time(time_t time) {
+  static char buf[FORMAT_TIME_BUF_CAP] = {};
+  struct tm *local = localtime(&time);
+  snprintf(buf, FORMAT_TIME_BUF_CAP, "%u:%u", local->tm_hour % 12, local->tm_min);
+  return buf;
+}
 
 int main(void) {
   signal(SIGQUIT, sig_handler);
   signal(SIGINT, sig_handler);
-  
-  while (running) {
+
+  while (true) {
     int cols, rows;
     get_term_size(&cols, &rows);
 
     printf(RESET_CURSOR);
     printf(CLEAR_SCREEN);
 
-    for (int i = 0; i < cols; ++i)
-      putchar('=');
-    printf(MOVE_CURSOR(1, rows));
-    for (int i = 0; i < cols; ++i)
-      putchar('=');
-    printf(MOVE_CURSOR(1, 2));
+    put_many_char('=', cols);
+    putchar('\n');
+
+    for (size_t i = 0; i < message_log.count; ++i)
+      printf(COLOR_GRAY"<%s : User>"COLOR_RESET" %s", format_time(message_log.items[i].time), message_log.items[i].msg);
     
+    printf(MOVE_CURSOR(1, rows - 1));
+    put_many_char('=', cols);
+    
+    putchar('\n');
     fgets(input_buf, INPUT_BUF_CAP, stdin);
 
-    if (strcmp(input_buf, "exit\n") == 0)
-      running = false;
+    if (strcmp(input_buf, "exit\n") == 0 || !input_buf[0])
+      break;
+    else if (input_buf[0] == '\n')
+      continue;
+    
+    nob_da_append(&message_log, ((message_t){time(NULL), strdup(input_buf)}));
+    memset(input_buf, '\0', INPUT_BUF_CAP);
   }
-  
+
   printf(RESET_CURSOR);
   printf(CLEAR_SCREEN);
+  
   return 0;
 }
